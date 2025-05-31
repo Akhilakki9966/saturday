bc6b1fb0465760b9ab6f45abe7fcdede29cbb8d5
Akhilakki9966 <Akhilakki9966@gmail.com>
2025-05-31 15:07:23 +0530

    Create Weekdays.java

---
+++ Weekdays.java
@@ -0,0 +1,10 @@
+import java.time.DayOfWeek;
+
+public class Weekdays {
+    public static void main(String[] args) {
+        // Loop through all days of the week
+        for (DayOfWeek day : DayOfWeek.values()) {
+            System.out.println(day);
+        }
+    }
+}
